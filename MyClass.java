ba0b6922e43504222c13c237b87e4d7a89851922
matiasdrianbacher.2006@gmail.com <matiasdrianbacher.2006@gmail.com>
2020-08-04 11:39:26 -0300

    agregos mas variables String y algunas numericas

--- MyClass.java
+++ MyClass.java
@@ -1,6 +1,10 @@
 public class MyClass {
     public static void main (String[] args){
-        String MiMensaje="hola mundo";
-        System.out.println(MiMensaje); //esto es un comentario como en C++ en una linea
+        String MiMensaje="hola";
+        String MiMensaje2="mundo";
+        int numero =2;
+        int numero2 =3;
+        int nuemro3 =numero+numero2;
+        System.out.println(MiMensaje+" "+MiMensaje2+nuemro3); //esto es un comentario como en C++ en una linea
     }
 }
